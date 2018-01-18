3e33e93b0c0765309fd360244aba28196b2f494f
volamtieutu <nhnhac15085@cusc.ctu.edu.vn>
2018-01-19 01:14:35 +0700

    project

---
+++ DrNguyenClinic/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DrNguyenClinic.MvcApplication" Language="C#" %>
---
+++ DrNguyenClinic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DrNguyenClinic.Startup))]
+namespace DrNguyenClinic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
